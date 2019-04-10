9c655e6c22322b3d8e292f435048ae6f1df357a6
Chris Callan <chriscallanjr@me.com>
2019-04-10 11:23:11 -0400

    ✏️ started writing some code

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
+const path = require('path')
+const clear = require('clear')
+const program = require('commander')
+
+clear()
+console.log(chalk.blue('TEST'))
+
+
